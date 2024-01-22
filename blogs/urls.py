22802e3be6a4bcfbae7f816b11a9d91413324808
sivaramsj <sivaramsj003@gmail.com>
2024-01-22 16:34:58 +0530

    add create and edit blog functionality

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import post_list,post_detail
+from .views import post_list,post_detail,post_new,post_edit
 urlpatterns = [
     path('',post_list,name='post_list'),
     path('post/<int:pk>/',post_detail,name='post_detail'),
+    path('post/new/',post_new,name='post_new'),
+    path('post/edit/<int:pk>/',post_edit,name='post_edit')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from django.utils import timezone
+
+
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +15,31 @@ def post_list(request):
 def post_detail(request,pk):
     post=Post.objects.get(pk=pk)
     return render(request,'blogs/post_detail.html',{'post':post})
+
+def post_new(request):
+    if request.method=='POST':
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.id)
+    else:
+        form=PostForm()
+    return render(request,'blogs/post_new.html',{'form':form})
+
+
+def post_edit(request,pk):
+    post=Post.objects.get(pk=pk)
+    if request.method=='POST':
+        form=PostForm(request.POST,instance=post)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            form.save()
+            return redirect('post_detail',pk=post.id)
+    else:
+        form=PostForm(instance=post)
+    return render(request,'blogs/post_new.html',{'form':form})
