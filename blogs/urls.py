fcb5243113bc304a935e883b986ec5e9b9298f04
sivaramsj <sivaramsj003@gmail.com>
2024-01-23 15:38:06 +0530

    comment portion is added to site

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import post_list,post_detail,post_new,post_edit
+from .views import post_list,post_detail,post_new,post_edit,post_draft,post_publish,post_remove,add_comment_to_post,comment_approve,comment_remove
 urlpatterns = [
     path('',post_list,name='post_list'),
     path('post/<int:pk>/',post_detail,name='post_detail'),
     path('post/new/',post_new,name='post_new'),
-    path('post/edit/<int:pk>/',post_edit,name='post_edit')
+    path('post/edit/<int:pk>/',post_edit,name='post_edit'),
+    path('draft/',post_draft,name='post_draft_list'),
+    path('post/publish/<int:pk>',post_publish,name='post_publish'),
+    path('post/remove/<int:pk>',post_remove,name='post_remove'),
+    path('post/comment/<int:pk>', add_comment_to_post, name='add_comment_to_post'),
+
+    path('comment/approve/<int:pk>', comment_approve, name='comment_approve'),
+    path('comment/remove/<int:pk>', comment_remove, name='comment_remove'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
-from .models import Post
-from .forms import PostForm
+from .models import Post,Comment
+from .forms import PostForm,CommentForm
 
 # Create your views here.
 
@@ -12,24 +13,26 @@ def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blogs/post_list.html',{'posts':posts})
 
+@login_required
 def post_detail(request,pk):
     post=Post.objects.get(pk=pk)
     return render(request,'blogs/post_detail.html',{'post':post})
 
+@login_required
 def post_new(request):
     if request.method=='POST':
         form=PostForm(request.POST)
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.published_date=timezone.now()
+            # post.published_date=timezone.now()
             post.save()
             return redirect('post_detail',pk=post.id)
     else:
         form=PostForm()
     return render(request,'blogs/post_new.html',{'form':form})
 
-
+@login_required
 def post_edit(request,pk):
     post=Post.objects.get(pk=pk)
     if request.method=='POST':
@@ -37,9 +40,54 @@ def post_edit(request,pk):
         if form.is_valid():
             post=form.save(commit=False)
             post.author=request.user
-            post.published_date=timezone.now()
+            # post.published_date=timezone.now()
             form.save()
             return redirect('post_detail',pk=post.id)
     else:
         form=PostForm(instance=post)
     return render(request,'blogs/post_new.html',{'form':form})
+
+@login_required
+def post_draft(request):
+    posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blogs/post_draft.html',{'posts':posts})
+
+@login_required
+def post_publish(request,pk):
+    post=Post.objects.get(pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=post.id)
+
+@login_required
+def post_remove(request,pk):
+    post=Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+def add_comment_to_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blogs/add_comment_to_post.html', {'form': form})
+
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
